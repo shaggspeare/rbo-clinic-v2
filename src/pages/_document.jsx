6df52a1c7498e4626dd2a65ac5c288e2286c61bf
slava <viacheslav.stetsenko@gmail.com>
2023-02-28 20:00:48 +0200

    add prettier, proper fonts

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,11 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700;800&family=Days+One&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
       </Head>
