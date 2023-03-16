ea5cc6248caf8a3a21d7b01f721d9cf9b5153ba6
slava <viacheslav.stetsenko@gmail.com>
2023-03-17 01:33:01 +0200

    refactor form

--- src/components/home/home/appointment.jsx
+++ src/components/home/home/appointment.jsx
@@ -1,6 +1,6 @@
-import AppointForm from "@/components/forms/appoint-form";
-import React from "react";
-import Image from "next/image";
+import AppointForm from '@/components/forms/appoint-form';
+import React from 'react';
+import Image from 'next/image';
 
 const Appointment = () => {
   return (
@@ -10,12 +10,7 @@ const Appointment = () => {
           <div className="row">
             <div className="col-xxl-6 col-xl-5 col-lg-12 col-md-12 p-0">
               <div className="appoinment-thumb">
-                <Image
-                    src="/assets/img/background/reception.jpeg"
-                    alt="appointment-img"
-                    width={1200}
-                    height={747}
-                />
+                <Image src="/assets/img/background/reception.jpeg" alt="appointment-img" width={1200} height={747} />
               </div>
             </div>
             <div className="col-xxl-6 col-xl-7 col-lg-12 col-md-12 p-0">
@@ -23,7 +18,9 @@ const Appointment = () => {
                 <h4 className="appointment-title mb-25">
                   <i className="fa-light fa-file-signature"></i>Записатися на прийом
                 </h4>
-                <p className="appointment-description mb-25">Будь-ласка, вкажіть свої дані і ми зв'яжемось з Вами для запису на зручний для Вас час </p>
+                <p className="appointment-description mb-25">
+                  Будь-ласка, вкажіть свої дані і ми зв&apos;яжемось з Вами для запису на зручний для Вас час
+                </p>
                 <div className="visitor-form">
                   {/* form start */}
                   <AppointForm />
