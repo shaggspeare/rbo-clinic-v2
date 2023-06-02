17026d65fd92a373f6c7b62b104da6e73c3b2fb3
slava <viacheslav.stetsenko@gmail.com>
2023-06-02 13:50:05 +0300

    add google tag manager

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -8,6 +8,18 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-V6XYYPFZZY`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-V6XYYPFZZY');
+              `,
+          }}
+        />
       </Head>
       <body>
         <Main />
